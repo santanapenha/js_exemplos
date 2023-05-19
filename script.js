f691059652c7710965d1244acaf47ed827ad2bb1
santanapenha <maria.santanapenha2006@gmail.com>
2023-05-19 14:13:50 -0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// var valor = confirm("Vamos juntos aprender a aprogramar em JS");
+// if (valor == true) {
+//     alert("Parabéns, vamos lá!!!");
+// } else {
+//     alert("Opção errada, apete F5 para recarregar a página.");
+// }
+
+var email = prompt("Digite seu e-mail");
+alert("O e-mail " + email + " foi coletado com sucesso.");

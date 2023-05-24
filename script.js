6449f8db94834b6475ff9ca6d35fea2917abc51e
santanapenha <maria.santanapenha2006@gmail.com>
2023-05-24 14:55:15 -0300

    update

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
 //     alert("Opção errada, apete F5 para recarregar a página.");
 // }
 
-var email = prompt("Digite seu e-mail");
-alert("O e-mail " + email + " foi coletado com sucesso.");
+// var email = prompt("Digite seu e-mail", "");
+// if (email == true) {
+//      alert("O e-mail " + email + " foi coletado com sucesso.");
+// }  else {
+//      alert("O e-mail não foi coletado.");
+// }
+
+ var prova = parseFloat(prompt("Qual a nota da prova?"));
+ var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+
+ var notaFinal = prova + trabalho;
+
+ alert(notaFinal);
